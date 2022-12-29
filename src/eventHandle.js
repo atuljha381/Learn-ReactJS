c8067ff36fb0c204fe9b30c24167a8255dd5348a
Jha <atul.a.jha@capgemini.com>
2022-12-29 11:04:16 +0530

    Handling Events Done

--- src/eventHandle.js
+++ src/eventHandle.js
@@ -1,15 +1,23 @@
-class Toggle extends React.Component{
-    constructor(props){
+import React from "react";
+
+export default class Toggle extends React.Component {
+    constructor(props) {
         super(props)
-        this.state = {isToggleOn: true}
+        this.state = { isToggleOn: true }
         this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick(){
-        this.state(prevState => ({
+    handleClick() {
+        this.setState(prevState => ({
             isToggleOn: !prevState.isToggleOn
         }))
     }
 
-
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import commentUse from "./ComponentChild";
 import Clock from "./usingStates";
 import ReactDOM from 'react-dom/client';
+import Toggle from './eventHandle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<Clock />)
+root.render(<Toggle />)
